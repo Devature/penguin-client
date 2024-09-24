ee53571df4cc15eecdd58f75a1f27333f5eb4af7
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-09-24 16:52:36 -0400

    Create axios instance for making fetch requests

--- src/ApiTest.tsx
+++ src/ApiTest.tsx
@@ -1,20 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { penguinApi } from "./util/axios";
 
 export default function ApiTest() {
-    const [msg, setMsg] = useState("");
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        fetch("http://localhost:8080", {
-            method: "GET", credentials: "include", headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(res => res.json())
-        .then(res => setMsg(res.message))
-        .catch(_ => setMsg("Something went wrong fetching from the API"));
-    }, []);
+  async function testApi() {
+    if (loading) return;
+    setLoading(true);
+    try {
+      const res = await penguinApi.get("/");
+      setMsg(res.data.message);
+    } catch (err) {
+      console.error(err);
+      setMsg("Something went wrong fetching from the API");
+    } finally {
+      setLoading(false);
+    }
+  }
 
-    return <div style={{backgroundColor: "blue", border: "1px solid red", padding: "5px"}}>
-        API test message: {msg}
-    </div>;
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginBottom: "1rem",
+        }}
+      >
+        <button
+          onClick={testApi}
+          style={{ marginRight: "1rem" }}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Test API"}
+        </button>
+        <button onClick={() => setMsg("")}>Clear test message</button>
+      </div>
+      <div
+        style={{
+          backgroundColor: "blue",
+          border: "1px solid red",
+          padding: "5px",
+        }}
+      >
+        API test message:
+        <br />
+        &nbsp;{msg}
+      </div>
+    </div>
+  );
 }
