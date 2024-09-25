d49c387e5e838b451702c830ec32b0d8fa5d4e93
Jason Fox <60980739+jsonfox@users.noreply.github.com>
2024-09-25 15:47:06 -0400

    Add material ui & set color pallette (#3)
    Co-authored-by: JtheFox <60980739+JtheFox@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import "./App.css";
 import ApiTest from "./ApiTest";
+import { Box, ThemeProvider } from "@mui/material";
+import { theme } from "./util/theme";
 
 function App() {
   return (
     <>
-      <ApiTest />
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            bgcolor: "background.default",
+            m: 0,
+            minWidth: "100vw",
+            minHeight: "100vh",
+          }}
+        >
+          <ApiTest />
+        </Box>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/util/theme.tsx
@@ -0,0 +1,39 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#F6F6F6",
+      secondary: "#DBE3E5",
+    },
+    primary: {
+      main: "#2B4753",
+      contrastText: "#F6F6F6",
+    },
+    secondary: {
+      light: "#D9B45D",
+      main: "#D0894B",
+      contrastText: "#DBE3E5",
+    },
+    background: {
+      default: "#253542",
+      paper: "#1a262fba",
+    },
+    error: {
+      main: "#ff5252",
+    },
+  },
+  components: {
+    MuiLink: {
+      defaultProps: {
+        color: "primary.light",
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+    },
+  },
+});
