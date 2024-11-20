d79b97af1dfd8876aaae5c732f6911f48f992751
Acohen455 <aaroncohen455@gmail.com>
2024-11-20 07:54:36 -0700

    Template files
    Added template files needed for the sign in template and removed the dud login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Index from './pages/Index';
 import ApiTest from './pages/ApiTest';
 import WorkIndex from './pages/WorkIndex';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import Layout from './components/Layout';  // Import the Layout component
 
 //this gets used by app.tsx
@@ -20,7 +20,7 @@ export const router = createBrowserRouter([
             { path: '/', element: <Index /> },
             { path: '/test', element: <ApiTest /> },
             { path: '/works', element: <WorkIndex /> },
-            { path: '/login', element: <Login /> },
+            { path: '/signin', element: <SignIn /> },
         ],
     },
 ]);
