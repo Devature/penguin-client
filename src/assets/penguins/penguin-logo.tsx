ec98a7aebc9dc81592d205db8fb004c5cf2a92c9
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-11-23 14:54:12 -0500

    Fix some logo svg stuff and add gabriola

--- src/assets/penguins/penguin-logo.tsx
+++ src/assets/penguins/penguin-logo.tsx
@@ -1,12 +1,15 @@
-import * as React from 'react';
 
 {/* Viewbox is what makes this tick -- it wraps the icon in a container with the viewbox size which then scales
     to the container -- this means that the icon scales to the appbar (etc.), instead of being cut off
     Conversion to Typescript was done with SVGR -- the SVG files are still present, if you need to transform the code
     again for some reason, just use SVGR on the SVG files BUT be sure to add the viewbox to ensure the raster scales*/}
-const SvgPenguinLogo = (props) => (
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 450 145" {...props}>
-
+const SvgPenguinLogo = ({ size }: SVGCustomIconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size / 3.103448275862069}
+        viewBox="0 0 450 145"
+    >
         <defs>
             <path
                 id="penguin-logo_svg__c"
@@ -34,9 +37,9 @@ const SvgPenguinLogo = (props) => (
             style={{
                 fontSize: 72,
                 fontFamily: 'Gabriola',
-                InkscapeFontSpecification: '&quot',
+
                 textAlign: 'start',
-                writingMode: 'lr-tb',
+                writingMode: 'horizontal-tb',
                 direction: 'ltr',
                 textAnchor: 'start',
                 fill: '#000',
@@ -48,7 +51,7 @@ const SvgPenguinLogo = (props) => (
             style={{
                 fontSize: 72,
                 fontFamily: 'Gabriola',
-                InkscapeFontSpecification: '&quot',
+
                 whiteSpace: 'pre',
             }}
             transform="translate(0 27.888)"
