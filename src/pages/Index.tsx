6df4795135d7ca654bbf5ccaa7efd1d5505d7d41
Acohen455 <aaroncohen455@gmail.com>
2024-11-24 07:48:40 -0700

    Adding a new homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,36 @@
-import { Button, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Hero from '../components/homepage/Hero';
+import LogoCollection from '../components/homepage/LogoCollection';
+import Highlights from '../components/homepage/Highlights';
+import Pricing from '../components/homepage/Pricing';
+import Features from '../components/homepage/Features';
+import Testimonials from '../components/homepage/Testimonials';
+import FAQ from '../components/homepage/FAQ';
+import Footer from '../components/homepage/Footer';
+import AppTheme from '../assets/template-themes/AppTheme';
 
-export default function Index() {
-    return (
-        <>
-            <Typography variant="h4" component="div">
-                Welcome to Ticket Penguin!
-            </Typography>
-            <Button component={Link} to={'/test'}>
-                Test API
-            </Button>
+export default function Index(props: { disableCustomTheme?: boolean }) {
+  return (
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
 
-            <Button component={Link} to={'/works'}>
-                Works Rest repository POC
-            </Button>
-        </>
-    );
+      <Hero />
+      <div>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </div>
+    </AppTheme>
+  );
 }
