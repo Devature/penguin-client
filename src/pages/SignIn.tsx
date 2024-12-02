f8a290baa50de629a87aa9ec2d9110978c390dfd
myang5128 <93555292+myang5128@users.noreply.github.com>
2024-12-02 15:27:25 -0500

    Remember Me Functionality (#46)
    * added remember me functionality using cookies, haven't tested
    
    * fixed error where email field wasn't being populated with cookie info

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -75,6 +75,7 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
   const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
   const [signInError, setSignInError] = React.useState('');
   const [open, setOpen] = React.useState(false);
+  const [rememberMe, setRememberMe] = React.useState(false);
 
 
   // Call useSignUpNavigation inside the component to follow hook rules
@@ -87,6 +88,10 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleRememberMeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRememberMe(e.target.checked);
+  }
 
 // client side input validation for email and password
   const validateInputs = (): boolean => {
@@ -143,6 +148,14 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
                 .then((isSuccess: boolean) => {
                     if (isSuccess) {
                         console.log("Success");
+                        if (rememberMe) {
+                            console.log("Cookie created")
+                            createCookie(email);
+                        }
+                        else {
+                            console.log("Deleted cookie if it exists");
+                            deleteCookie();
+                        }
                         navigateHome();
                     } else {
                         console.error("Login unsuccessful");
@@ -150,6 +163,38 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
                 });
         }
     };
+
+    const createCookie = (email: string) : void => {
+        const date = new Date();
+        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        const expires = `expires=${date.toUTCString()}`;
+        document.cookie = `rememberEmail=${email}; ${expires}; path=/`;
+        console.log(`Cookie Name: ${email}, Expiration Date: ${expires}`);
+    }
+
+    const deleteCookie = (): void => {
+        document.cookie = "rememberEmail=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    }
+
+    const findCookie = (): string | null => {
+        const cookies = document.cookie.split('; ');
+        for (const cookie of cookies) {
+            const [name, email] = cookie.split('=');
+            if (name === 'rememberEmail') {
+                return decodeURIComponent(email);
+            }
+        }
+        return null;
+    }
+
+    React.useEffect(() => {
+        const savedEmail = findCookie();
+        if (savedEmail) {
+            console.log("Cookies found");
+            setEmail(savedEmail);
+            setRememberMe(true);
+        }
+    }, []);
 
 
     const navigateHome = useDashboardNavigation();
@@ -209,6 +254,7 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
                             variant="outlined"
                             color={emailError ? 'error' : 'primary'}
                             onChange={(e) => setEmail(e.currentTarget.value)}
+                            value={email}
                         />
                     </FormControl>
                     <FormControl>
@@ -230,7 +276,7 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
                     </FormControl>
                     <FormControlLabel
                         control={
-                            <Checkbox value="remember" color="primary" />
+                            <Checkbox value="remember" color="primary" checked={rememberMe} onChange={handleRememberMeChange}/>
                         }
                         label="Remember me"
                     />
